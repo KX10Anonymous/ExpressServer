6d6e0a935ebd5a013e3ce2c8b5e2644d7fe0e521
KeituTshwale <Keitumetsetshwale661@gmail.com>
2024-08-20 12:24:20 +0200

    Testing

--- index.js
+++ index.js
@@ -6,32 +6,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/application', (req, res) => {
-    const { name, email, password, loanAmount, term } = req.body;
+    const {idNumber, loanAmount, term } = req.body;
 
-    const annualRate = 6;
+    console.log(req.body);
+
+    const annualRate = 8;
     const monthlyRate = annualRate / 100 / 12;
 
     const numberOfPayments = parseInt(term);
     const loanAmountFloat = parseFloat(loanAmount);
 
-    if (name && email && password && !isNaN(loanAmountFloat) && !isNaN(numberOfPayments)) {
+    
         const denominator = Math.pow(1 + monthlyRate, numberOfPayments) - 1;
         const monthlyPayment = (loanAmountFloat * monthlyRate * Math.pow(1 + monthlyRate, numberOfPayments)) / denominator;
 
         return res.json({
             status: "Application Submitted",
-            name: name,
-            email: email,
+            idNumber:idNumber,
             loanAmount: `R${loanAmountFloat.toFixed(2)}`,
             term: `${term} months`,
             annualInterestRate: `${annualRate}%`,
             monthlyPayment: `R${monthlyPayment.toFixed(2)}`
         });
-    } else {
+
         return res.status(400).json({
             error: 'Please fill in all fields correctly.'
         });
-    }
+    
 });
 
 app.listen(port, () => {
